4cbe0e6b1e89a81c793f78dc6f3209108e57ac88
Raiqa Rasool <rasoolraiqa@gmail.com>
2023-12-14 17:12:24 +0500

    product models added

--- django_6pm/django_6pm/product/models.py
+++ django_6pm/django_6pm/product/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=250)
+    brand_logo = models.URLField()
+
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length=250)
+    parent_cat_id = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Product(models.Model):
+    product_id = models.PositiveBigIntegerField(primary_key=True)
+    name = models.CharField(max_length=1000)
+    gender = models.CharField(max_length=100)
+    description = models.TextField()
+    currency = models.CharField(max_length=50)
+    brand_id = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True)
+    cat_id = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Sku(models.Model):
+    sku_size = models.CharField(max_length=250)
+    sku_color = models.CharField(max_length=1000)
+    sku_price = models.FloatField()
+    previous_price = models.FloatField()
+    out_of_stock = models.BooleanField()
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Sku_image(models.Model):
+    sku_id = models.ForeignKey(Sku, on_delete=models.CASCADE)
+    image_url = models.URLField()
