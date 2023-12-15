af828dd2c523371ba6a0dc07067d896e11dff817
Raiqa Rasool <rasoolraiqa@gmail.com>
2023-12-15 12:11:12 +0500

    Product models modified

--- django_6pm/django_6pm/product/models.py
+++ django_6pm/django_6pm/product/models.py
@@ -2,34 +2,42 @@ from django.db import models
 
 
 class Brand(models.Model):
-    brand_name = models.CharField(max_length=250)
-    brand_logo = models.URLField()
+    name = models.CharField(max_length=250)
+    logo = models.URLField()
 
 
 class Category(models.Model):
-    cat_name = models.CharField(max_length=250)
-    parent_cat_id = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=250)
+    parent = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True)
 
 
 class Product(models.Model):
-    product_id = models.PositiveBigIntegerField(primary_key=True)
+    gender_choices = [
+        ('women', 'Women'),
+        ('men', 'Men'),
+        ('unisex-adults', 'Unisex Adults'),
+        ('boys','Boys'),
+        ('girls', 'Girls'),
+        ('unisex-kids', 'Unisex Kids')
+    ]
+
     name = models.CharField(max_length=1000)
-    gender = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100, choices=gender_choices)
     description = models.TextField()
     currency = models.CharField(max_length=50)
-    brand_id = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True)
-    cat_id = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
 
 class Sku(models.Model):
-    sku_size = models.CharField(max_length=250)
-    sku_color = models.CharField(max_length=1000)
-    sku_price = models.FloatField()
+    size = models.CharField(max_length=250)
+    color = models.CharField(max_length=1000)
+    price = models.FloatField()
     previous_price = models.FloatField()
-    out_of_stock = models.BooleanField()
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    is_in_stock = models.BooleanField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
 class Sku_image(models.Model):
-    sku_id = models.ForeignKey(Sku, on_delete=models.CASCADE)
-    image_url = models.URLField()
+    sku = models.ForeignKey(Sku, on_delete=models.CASCADE)
+    image = models.URLField()
