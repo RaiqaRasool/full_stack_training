ea88283eb80e6fa70a6503f36f8e702307b525ca
Raiqa Rasool <rasoolraiqa@gmail.com>
2023-12-18 12:35:43 +0500

    related_name added to all models, charfield length set in 2 power

--- django_6pm/django_6pm/product/models.py
+++ django_6pm/django_6pm/product/models.py
@@ -3,13 +3,13 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=256)
     logo = models.URLField()
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=250)
-    parent = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=256)
+    parent = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True, related_name="children")
 
 
 class Product(models.Model):
@@ -24,24 +24,24 @@ class Product(models.Model):
     name = models.TextField()
     gender = models.IntegerField(choices=Gender.choices)
     description = models.TextField()
-    currency = models.CharField(max_length=10)
-    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    currency = models.CharField(max_length=16)
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
 
 
-class ProdColor(models.Model):
-    color = models.TextField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+class ProductColor(models.Model):
+    color = models.CharField(max_length=128)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="colors")
 
 
-class ProdColorSize(models.Model):
-    size = models.CharField(max_length=100)
+class ProductColorSize(models.Model):
+    size = models.CharField(max_length=64)
     price = models.FloatField()
     previous_price = models.FloatField()
     is_in_stock = models.BooleanField(default=True)
-    prod_color = models.ForeignKey(ProdColor, on_delete=models.CASCADE)
+    color = models.ForeignKey(ProductColor, on_delete=models.CASCADE, related_name="sizes")
 
 
 class ProdColorImage(models.Model):
     image = models.URLField()
-    prod_color = models.ForeignKey(ProdColor, on_delete=models.CASCADE)
+    color = models.ForeignKey(ProductColor, on_delete=models.CASCADE, related_name='images')
