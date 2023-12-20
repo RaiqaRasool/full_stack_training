35a960a716d0e00f6cfa6bd88efe73e564338fd9
Raiqa Rasool <rasoolraiqa@gmail.com>
2023-12-20 14:35:07 +0500

    utils for product created

---
+++ django_6pm/django_6pm/product/utils/utils.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+from slugify import slugify
+
+
+def validate_float(data: str) -> float:
+    return float(data.replace(",", ""))
+
+
+def generate_slug(title: str) -> Any:
+    return slugify(title)
