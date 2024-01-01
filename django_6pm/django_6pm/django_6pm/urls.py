621604f5d56c8c1b28d0a269b40533bbbf8e269b
Raiqa Rasool <rasoolraiqa@gmail.com>
2024-01-01 16:38:57 +0500

    Product app views completed

--- django_6pm/django_6pm/django_6pm/urls.py
+++ django_6pm/django_6pm/django_6pm/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,4 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("user.urls")),
+    path("", include("product.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- django_6pm/django_6pm/product/views.py
+++ django_6pm/django_6pm/product/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
+from product.models import Category, Product, Brand
+from django.db.models import QuerySet
+from typing import Any, Dict
 
-# Create your views here.
+
+class CategoriesListView(ListView):
+    context_object_name = "categories"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        categories = Category.objects.filter(parent_id=None)
+        return categories
+
+
+class BrandsListView(ListView):
+    model = Brand
+    context_object_name = "brands"
+
+
+class BrandProductListView(ListView):
+    template_name = "product/products_by_brand.html"
+    context_object_name = "products"
+    paginate_by = 20
+
+    def get_queryset(self) -> QuerySet[Any]:
+        self.brand = get_object_or_404(Brand, slug=self.kwargs["brand_slug"])
+        self.products = self.brand.products.all()
+        return self.products
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["brand"] = self.brand
+        context["total_products"] = len(self.products)
+        return context
+
+
+class CategoryProductListView(ListView):
+    template_name = "product/products_by_category.html"
+    context_object_name = "products"
+    paginate_by = 20
+
+    def get_queryset(self) -> QuerySet[Any]:
+        self.category = get_object_or_404(Category, slug=self.kwargs["category_slug"])
+        self.subcategories = self.category.subcategories.all()
+        self.products = (
+            Product.objects.filter(category=self.category).select_related("brand").prefetch_related("sizes", "images")
+        )
+        return self.products
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["category"] = self.category
+        context["subcategories"] = self.subcategories
+        context["total_products"] = len(self.products)
+        return context
+
+
+class ProductDetailView(DetailView):
+    context_object_name = "product"
+
+    def get_queryset(self):
+        return Product.objects.filter(retailer_sku=self.kwargs["pk"]).select_related().prefetch_related("images", "sizes")
