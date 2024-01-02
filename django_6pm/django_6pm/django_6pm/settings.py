51dff0dc3481f9d9bdbe770fa009defee78fd715
Raiqa Rasool <rasoolraiqa@gmail.com>
2024-01-02 16:50:12 +0500

    Homepage and detailed listing of navbar added

--- django_6pm/django_6pm/django_6pm/settings.py
+++ django_6pm/django_6pm/django_6pm/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "user.context_processors.profile"
+                "product.context_processors.navbar_data"
             ],
         },
     },
--- django_6pm/django_6pm/product/urls.py
+++ django_6pm/django_6pm/product/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from product.views import *
 
 urlpatterns = [
-    path("", CategoriesListView.as_view(), name="categories"),
+    path("", HomeListView.as_view(), name="home"),
+    path("g/<gender>", GenderProductListView.as_view(), name="gender_products"),
+    path("categories", CategoriesListView.as_view(), name="categories"),
     path("c/<category_slug>", CategoryProductListView.as_view(), name="category_products"),
     path("brands", BrandsListView.as_view(), name="brands"),
     path("b/<brand_slug>", BrandProductListView.as_view(), name="brand_products"),
