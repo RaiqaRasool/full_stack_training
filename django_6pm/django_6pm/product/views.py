82775a64f0f3be1ab93bcac6744198f1a2fc9fdb
Raiqa Rasool <rasoolraiqa@gmail.com>
2024-01-02 16:50:12 +0500

    Homepage and detailed listing of navbar added

--- django_6pm/django_6pm/product/views.py
+++ django_6pm/django_6pm/product/views.py
@@ -1,8 +1,56 @@
-from django.shortcuts import get_object_or_404
-from django.views.generic import ListView, DetailView
-from product.models import Category, Product, Brand
-from django.db.models import QuerySet
 from typing import Any, Dict
+
+from django.db.models import Count, QuerySet
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, ListView
+
+from product.models import Brand, Category, Product
+
+
+class HomeListView(ListView):
+    template_name = "product/home.html"
+    context_object_name = "category_products_mapping"
+    model = Category
+
+    def get_category_products(self, cat, products):
+        subcats = list(cat.subcategories.all())
+        if subcats is None or len(products) >= 4:
+            return products
+        for subcat in subcats:
+            subcat_prods = list(subcat.products.select_related().prefetch_related("images", "sizes")[:4])
+            if subcat_prods:
+                products.extend(subcat_prods)
+            if len(products) >= 4:
+                return products
+            products = self.get_category_products(subcat, products)
+        return products
+
+    def get_categories_products(self):
+        categories = Category.objects.filter(parent_id=None)
+        category_product_mapping = {}
+        for category in categories:
+            category_product_mapping[category] = self.get_category_products(category, [])
+        return category_product_mapping
+
+    def get_genders(self):
+        gender_queryset = Product.objects.values("gender").order_by("gender").distinct()
+        gender_values = [item["gender"] for item in gender_queryset]
+        return gender_values
+
+    def get_genders_products(self):
+        genders = self.get_genders()
+        gender_products_mapping = {}
+        for gender in genders:
+            gender_products_mapping[Product.Gender(gender).label] = (
+                Product.objects.filter(gender=gender).select_related().prefetch_related("sizes", "images")[0:4]
+            )
+        return gender_products_mapping
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["category_products_mapping"] = self.get_categories_products()
+        context["gender_products_mappings"] = self.get_genders_products()
+        return context
 
 
 class CategoriesListView(ListView):
@@ -16,6 +64,24 @@ class CategoriesListView(ListView):
 class BrandsListView(ListView):
     model = Brand
     context_object_name = "brands"
+
+
+class GenderProductListView(ListView):
+    template_name = "product/products_by_gender.html"
+    context_object_name = "products"
+    paginate_by = 20
+
+    def get_queryset(self) -> QuerySet[Any]:
+        self.gender = self.kwargs["gender"]
+        gender_idx = self.gender.upper().replace("-", "_")
+        self.products = Product.objects.filter(gender=Product.Gender[gender_idx])
+        return self.products
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context["gender"] = self.gender
+        context["total_products"] = len(self.products)
+        return context
 
 
 class BrandProductListView(ListView):
@@ -60,4 +126,6 @@ class ProductDetailView(DetailView):
     context_object_name = "product"
 
     def get_queryset(self):
-        return Product.objects.filter(retailer_sku=self.kwargs["pk"]).select_related().prefetch_related("images", "sizes")
+        return (
+            Product.objects.filter(retailer_sku=self.kwargs["pk"]).select_related().prefetch_related("images", "sizes")
+        )
