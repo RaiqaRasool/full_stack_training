2686e7a76a7584fd351db33c876a73b83cd439ef
Raiqa Rasool <rasoolraiqa@gmail.com>
2024-01-03 15:41:05 +0500

    Product Views urls made consistent

--- django_6pm/django_6pm/product/urls.py
+++ django_6pm/django_6pm/product/urls.py
@@ -4,10 +4,10 @@ from product.views import *
 
 urlpatterns = [
     path("", HomeListView.as_view(), name="home"),
-    path("g/<gender>", GenderProductListView.as_view(), name="gender_products"),
+    path("genders/<gender>", GenderProductListView.as_view(), name="gender_products"),
     path("categories", CategoriesListView.as_view(), name="categories"),
-    path("c/<category_slug>", CategoryProductListView.as_view(), name="category_products"),
+    path("categories/<category_slug>", CategoryProductListView.as_view(), name="category_products"),
     path("brands", BrandsListView.as_view(), name="brands"),
-    path("b/<brand_slug>", BrandProductListView.as_view(), name="brand_products"),
-    path("p/<pk>", ProductDetailView.as_view(), name="product"),
+    path("brands/<brand_slug>", BrandProductListView.as_view(), name="brand_products"),
+    path("product/<pk>", ProductDetailView.as_view(), name="product"),
 ]
