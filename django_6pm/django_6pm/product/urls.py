0fea8773440733e3c3a4c741a28325a83447200e
Raiqa Rasool <rasoolraiqa@gmail.com>
2024-01-12 14:36:26 +0500

    URLS slash location improved

--- django_6pm/django_6pm/product/urls.py
+++ django_6pm/django_6pm/product/urls.py
@@ -4,24 +4,24 @@ from product.views import *
 
 category_urls = [
     path("", CategoriesListView.as_view(), name="categories"),
-    path("/<str:category_slug>", CategoryProductListView.as_view(), name="category_products"),
+    path("<str:category_slug>", CategoryProductListView.as_view(), name="category_products"),
 ]
 
 brand_urls = [
     path("", BrandsListView.as_view(), name="brands"),
-    path("/<str:brand_slug>", BrandProductListView.as_view(), name="brand_products"),
+    path("<str:brand_slug>", BrandProductListView.as_view(), name="brand_products"),
 ]
 
 gender_urls = [
-    path("/<str:gender>", GenderProductListView.as_view(), name="gender_products"),
+    path("<str:gender>", GenderProductListView.as_view(), name="gender_products"),
 ]
 
-product_urls = [path("/<int:pk>", ProductDetailView.as_view(), name="product")]
+product_urls = [path("<int:pk>", ProductDetailView.as_view(), name="product")]
 
 urlpatterns = [
     path("", HomeListView.as_view(), name="home"),
-    path("categories", include(category_urls)),
-    path("brands", include(brand_urls)),
-    path("genders", include(gender_urls)),
-    path("products", include(product_urls)),
+    path("categories/", include(category_urls)),
+    path("brands/", include(brand_urls)),
+    path("genders/", include(gender_urls)),
+    path("products/", include(product_urls)),
 ]
