f39a1045f07c8f81d48668edbdab3f90cf9254a7
Raiqa Rasool <rasoolraiqa@gmail.com>
2024-01-12 14:50:46 +0500

    Profile Searching for superuser stopped

--- django_6pm/django_6pm/user/context_processors.py
+++ django_6pm/django_6pm/user/context_processors.py
@@ -3,6 +3,6 @@ from user.models import Profile
 
 def profile(request):
     profile = None
-    if request.user.is_authenticated:
+    if request.user.is_authenticated and not request.user.is_superuser:
         profile = Profile.objects.get(user_id=request.user.id)
     return {"profile": profile}
