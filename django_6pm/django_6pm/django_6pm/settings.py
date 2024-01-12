7521d57020d9407da54a397f559d41c18abf2eb5
Raiqa Rasool <rasoolraiqa@gmail.com>
2024-01-12 16:42:30 +0500

    Django debug toolbar added

--- django_6pm/django_6pm/django_6pm/settings.py
+++ django_6pm/django_6pm/django_6pm/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "product",
     "user",
 ]
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
 ]
 
 ROOT_URLCONF = "django_6pm.urls"
@@ -107,6 +109,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
+]
+
+INTERNAL_IPS = [
+   '127.0.0.1',
 ]
 
 
