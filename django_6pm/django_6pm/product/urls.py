1b2ba460e577684f8b6e45e7361a6d817930657a
Raiqa Rasool <rasoolraiqa@gmail.com>
2024-01-12 15:04:38 +0500

    All News Views urls added

--- django_6pm/django_6pm/product/urls.py
+++ django_6pm/django_6pm/product/urls.py
@@ -16,7 +16,15 @@ gender_urls = [
     path("<str:gender>", GenderProductListView.as_view(), name="gender_products"),
 ]
 
-product_urls = [path("<int:pk>", ProductDetailView.as_view(), name="product")]
+product_urls = [
+    path("featured", FeaturedProductListView.as_view(), name="featured_products"),
+    path("inactive", InActiveProductsListView.as_view(), name="inactive_products"),
+    path("<int:pk>", ProductDetailView.as_view(), name="product"),
+    path("<int:pk>/edit", ProductUpdateView.as_view() , name="product_edit"),
+    path("<int:pk>/delete",  ProductDeleteView.as_view() , name="product_delete"),
+    path("<int:pk>/sku/<int:sku_id>", SkuSubscriptionCreateView.as_view(), name="product_sku_subscription"),
+    path("delete-success", ProductDeleteSuccessTemplateView.as_view(), name="product_delete_success")
+]
 
 urlpatterns = [
     path("", HomeListView.as_view(), name="home"),
